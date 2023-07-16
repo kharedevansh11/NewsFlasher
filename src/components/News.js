43da715cecef4a235dd5695ba221305131e05b3e
kharedevansh11 <devanshkhare11@gmail.com>
2023-07-16 23:06:57 +0530

    code_improvement

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,6 @@ import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import "./style.css"
 import InfiniteScroll from "react-infinite-scroll-component";
-
 const News = (props)=>{
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
