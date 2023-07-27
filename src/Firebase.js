695a4be11dd985ac7f0062ebd4a996bd391d4530
kharedevansh11 <devanshkhare11@gmail.com>
2023-07-28 00:10:05 +0530

    deploy on firebase

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCY5uW91s8OFdeXi3IxZ8ZMD-Wkhh45aCM",
+    authDomain: "news-flasher-f6778.firebaseapp.com",
+    projectId: "news-flasher-f6778",
+    storageBucket: "news-flasher-f6778.appspot.com",
+    messagingSenderId: "1055693748779",
+    appId: "1:1055693748779:web:0d031578106a9be7cf886e"
+  };
